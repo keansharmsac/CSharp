0a3de066439d6ee931f615dcf891b714c383a1ff
Sachin Sharma <61662388+sharmsac@users.noreply.github.com>
2022-01-21 13:43:15 -0500

    commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+String text = System.IO.File.ReadAllText("test.txt");
+Console.WriteLine(text);
+Console.WriteLine("Sachin Sharma");
+Console.WriteLine("Version: {0}", Environment.Version.ToString());
